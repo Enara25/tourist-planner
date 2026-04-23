1c4b811f47fff0535af9c4bf770717abf0307202
Enara Wicks <enarawicks@gmail.com>
2026-04-23 17:28:03 +0530

    Updating Database Credentials

--- server/db.js
+++ server/db.js
@@ -1,7 +1,11 @@
 // server/db.js
+require('dotenv').config();
 const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
-  host: 'localhost', user: 'root', password: '', database: 'tourist_planner',
+  host: process.env.host, 
+  user: process.env.user, 
+  password: process.env.password, 
+  database: process.env.database,
   waitForConnections: true, connectionLimit: 10
 });
 pool.getConnection()
